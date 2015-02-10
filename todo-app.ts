f25a94d5df0d19fca78904836ab26584b5668a32
Chris Bowdon <c.bowdon@bath.edu>
2015-02-10 22:19:28 +0000

    Removed any from todo-app

--- todo-app.ts
+++ todo-app.ts
@@ -1,43 +1,44 @@
 /// <reference path="mithril.d.ts" />
 // This is the todolist example from http://lhorie.github.io/mithril/getting-started.html
 
-var todo = {
-
-    //the Todo class has two properties
-    Todo: function(data: any) {
+class Todo { // We've lost the namespacing compared to the original,
+             // but that's easily fixed with a TypeScript module
+    description: MithrilProperty<string>;
+    done: MithrilProperty<boolean>;
+    constructor(data: { description: string }) {
         this.description = m.prop(data.description);
         this.done = m.prop(false);
-    },
+    }
+}
 
-    //the TodoList class is a list of Todo's
-    TodoList: Array,
+interface TodoCtrl extends MithrilController {
+    list: Todo[];
+    description: MithrilProperty<string>;
+    add(): void;
+}
 
-    //the controller uses three model-level entities, of which one is a custom defined class:
-    //`Todo` is the central class in this application
-    //`list` is merely a generic array, with standard array methods
-    //`description` is a temporary storage box that holds a string
-    //
-    //the `add` method simply adds a new todo to the list
-    controller: function() {
-        this.list = new todo.TodoList();
+var todo: MithrilModule<TodoCtrl> = {
+
+    // the cast to TodoCtrl is a bit ugly, but best we can do
+    controller: <TodoCtrl>function() {
+        this.list = []
         this.description = m.prop("");
 
         this.add = function() {
             if (this.description()) {
-                this.list.push(new (<any>todo.Todo)({description: this.description()}));
+                this.list.push(new Todo({description: this.description()}));
                 this.description("");
             }
         }.bind(this);
     },
 
-    //here's the view
-    view: function(ctrl: any) {
+    view: function(ctrl: TodoCtrl) {
         return m("html", [
             m("body", [
                 m("input", {onchange: m.withAttr("value", ctrl.description), value: ctrl.description()}),
                 m("button", {onclick: ctrl.add}, "Add"),
                 m("table", [
-                    ctrl.list.map(function(task: any) {
+                    ctrl.list.map(function(task: Todo) {
                         return m("tr", [
                             m("td", [
                                 m("input[type=checkbox]", {onclick: m.withAttr("checked", task.done), checked: task.done()})
@@ -51,5 +52,4 @@ var todo = {
     },
 };
 
-//initialize the application
 m.module(document, todo);
